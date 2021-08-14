587b5adaa0ca212d925a20d04886b8d0fcd05ba3
derek <derekle.creative@gmail.com>
2021-08-14 01:11:12 -0500

    commit tree 0c0a4ce897cd2587b3eb964ea9fed8d827c8d463

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(a){
+    return a.slice(0,2)
+}
+
+function returnLastTwoDrivers(a){
+    return a.slice(a.length-2, a.length)
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+
+function createFareMultiplier(int) {
+    console.log(int)
+    return function(f){
+        console.log(f)
+        return int * f
+    }
+}
+
+function fareDoubler(f){
+    return f * 2
+}
+function fareTripler(f){
+    return f * 3
+}
+
+function selectDifferentDrivers(a, f) {
+    console.log(a)
+    console.log(f)
+    return f(a)
+}
